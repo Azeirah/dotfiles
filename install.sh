f9fdcefee43099d8dad1a20aa5ed00230142c5d3
martijn <email@martijnbrekelmans.com>
2017-08-25 06:07:24 +0200

    update install

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
+sudo add-apt-repository ppa:ubuntu-mozilla-daily/firefox-aurora
 sudo apt update
 sudo apt install -y nodejs
 sudo apt install -y nodejs-legacy
 sudo apt install -y npm
 sudo apt install -y build-essential libssl-dev
+# firefox dev edition
+sudo apt install firefox
